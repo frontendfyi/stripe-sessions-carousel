3346de076bc24f0e629c3db5fddc2057a5e79ea1
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-04-22 17:33:50 +0200

    Video endresult

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import classNames from "classnames";
 import "./index.css";
-import { useEffect, useRef, useState } from "react";
+import classNames from "classnames";
 import { persons } from "./persons";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const [activeIndex, setActiveIndex] = useState(3);
-  const transitionTimeoutRef = useRef<number | null>(null);
-  const wrapperRef = useRef<HTMLUListElement>(null);
+  const [activeItem, setActiveItem] = useState(3);
+  const wrapperRef = useRef<HTMLUListElement | null>(null);
+  const timeoutRef = useRef<number | null>(null);
 
   useEffect(() => {
     if (!wrapperRef.current) return;
-    if (transitionTimeoutRef.current)
-      clearTimeout(transitionTimeoutRef.current);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
 
     wrapperRef.current.style.setProperty(
       "--transition",
       "600ms cubic-bezier(0.22, 0.61, 0.36, 1)"
     );
 
-    transitionTimeoutRef.current = setTimeout(() => {
-      if (!wrapperRef.current) return;
-      wrapperRef.current.style.removeProperty("--transition");
+    timeoutRef.current = setTimeout(() => {
+      wrapperRef.current?.style.removeProperty("--transition");
     }, 900);
 
     return () => {
-      if (transitionTimeoutRef.current)
-        clearTimeout(transitionTimeoutRef.current);
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
     };
-  }, [activeIndex]);
+  }, [activeItem]);
 
   return (
     <div className="flex h-full w-full items-center justify-center">
@@ -38,33 +39,35 @@ function App() {
         >
           {persons.map((person, index) => (
             <li
-              onClick={() => setActiveIndex(index)}
-              aria-current={activeIndex === index}
-              key={person.name}
+              onClick={() => setActiveItem(index)}
+              aria-current={activeItem === index}
               className={classNames(
-                "md:before-block relative w-full cursor-pointer before:absolute before:bottom-0 before:left-[-10px] before:right-[-10px] before:top-0 before:hidden before:bg-white md:h-auto md:[transition:width_var(--transition,200ms_ease-in)] md:first:w-[1%] md:last:w-[1%] md:[&:first-child_img]:opacity-0 md:[&:last-child_img]:opacity-0",
-                activeIndex === index
-                  ? "md:group-hover:not(:hover):w-[43%] md:w-[48%]"
-                  : "md:w-[8%] md:group-hover:hover:[&:not(:first-child,:last-child)]:w-[12%] md:group-hover:[&:not(:first-child,:last-child,>[aria-current='true'],:has(>[aria-current='true']))]:w-[7%]"
+                "relative md:w-[8%] md:first:w-[1%] md:last:w-[1%] md:[&[aria-current='true']]:w-[48%]",
+                "md:[transition:width_var(--transition,200ms_ease-in)]",
+                "md:before-block before:absolute before:bottom-0 before:left-[-10px] before:right-[-10px] before:top-0 before:hidden before:bg-white",
+                "md:[&:not(:hover),&:not(:first),&:not(:last)]:group-hover:w-[7%] md:hover:w-[12%]",
+                "first:pointer-events-none last:pointer-events-none md:[&_img]:first:opacity-0 md:[&_img]:last:opacity-0"
               )}
+              key={person.name}
             >
               <div className="relative h-full w-full overflow-hidden rounded-2xl bg-[#c9c6c7]">
                 <img
-                  className="absolute right-0 h-full w-24 object-cover object-center opacity-80 grayscale md:left-1/2 md:h-[640px] md:w-[590px] md:max-w-none md:-translate-x-1/2 md:transition-opacity"
+                  className="absolute right-0 top-1/2 h-auto w-24 max-w-none -translate-y-1/2 object-cover grayscale md:left-1/2 md:h-[640px] md:w-[590px] md:-translate-x-1/2"
+                  src={person.img}
+                  alt={person.name}
                   width="590px"
                   height="640px"
-                  src={person.img}
                 />
                 <div
                   className={classNames(
                     "inset-0 opacity-25 duration-300 before:absolute before:bottom-0 before:left-[-546px] before:right-0 before:top-[-148px] before:z-10 before:bg-texture  after:bottom-[28px] after:left-0 after:right-[-434px] after:top-0 after:z-10 after:bg-texture md:absolute md:transition-opacity",
-                    activeIndex === index ? "md:opacity-25" : "md:opacity-0"
+                    activeItem === index ? "md:opacity-25" : "md:opacity-0"
                   )}
                 />
                 <div
                   className={classNames(
-                    "w-[60%] p-4 [transition-property:transform,opacity] md:absolute md:left-8 md:top-8 md:w-[590px] md:p-0 md:[transition:var(--transition,300ms_ease-in-out)]",
-                    activeIndex === index
+                    "left-8 top-8 w-[590px] p-4 transition-[transform,opacity] md:absolute md:p-0",
+                    activeItem === index
                       ? "md:translate-x-0 md:opacity-100"
                       : "md:translate-x-4 md:opacity-0"
                   )}
